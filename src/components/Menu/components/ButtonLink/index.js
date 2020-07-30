7e5c2ccea4c110af0f7711e7dc0165e7a0a58556
carolhybank <caroline.silva@hybank.biz>
2020-07-30 18:44:28 -0300

    Added props

--- src/components/Menu/components/ButtonLink/index.js
+++ src/components/Menu/components/ButtonLink/index.js
@@ -1,7 +1,12 @@
 import React from  'react' ;
 
-function ButtonLink (){
+function ButtonLink (props){
+    //Props => {}
     return (
-        
-    )
+        <a className="Nada" href="/">
+            Novo vídeo
+        </a>
+    );
 }
+
+export default ButtonLink;
